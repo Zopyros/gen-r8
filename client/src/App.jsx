d6df20c5220fbceef1a04bfbfe6964b9f63b7836
Christian <cmanalo.io@outlook.com>
2023-06-03 21:39:36 -0700

    add logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,Link,Route,Routes } from 'react-router-dom';
+import {Home, CreatePost} from './pages';
+import logo from './assets/logo.jpg';
+
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold udnerline'>
-    Hello world!
-    </h1>
+
+   
+    <BrowserRouter>
+    <header className='w-full flex 
+    justify-between items-center bg-white 
+    sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+    </header>
+    <Link to="/">
+    <img src={logo} alt="logo" className=' w-40 object contain'></img>
+
+    </Link>
+
+    </BrowserRouter>
   )
 }
 
