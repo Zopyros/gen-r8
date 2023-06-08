a2bee693acf604d3041fa530d0c7d28dd3f41c14
Christian <cmanalo.io@outlook.com>
2023-06-07 22:16:46 -0700

    construct a base for the create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,8 +5,24 @@ import {getRandomPrompt} from '../utils'
 import { FormField,Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const[form,setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+
+  });
+  const [generatingImg,setGeneratingImg] = useState(false);
+  const [loading,setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+    <div>
+      <h1 className='font-extrabold text-[#36454F] text-[32px]'> Create</h1>
+      <p className='mt-2 text-[#36454F] text-[16px] max-w[500px]'> Create imaginative
+     and visually stunning images through Gen R8 AI and share them with the community</p>
+    </div>
+    </section>
   )
 }
 
