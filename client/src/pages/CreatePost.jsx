d7d0c12a869809246b29c05fee26e0f9c6866d99
Christian <cmanalo.io@outlook.com>
2023-06-08 21:04:01 -0700

    create image generating placeholder

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,6 +15,19 @@ const CreatePost = () => {
   const [generatingImg,setGeneratingImg] = useState(false);
   const [loading,setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+  
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
+
   return (
     <section className='max-w-7xl mx-auto'>
     <div>
@@ -22,6 +35,60 @@ const CreatePost = () => {
       <p className='mt-2 text-[#36454F] text-[16px] max-w[500px]'> Create imaginative
      and visually stunning images through Gen R8 AI and share them with the community</p>
     </div>
+
+    <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+    <div className='flex flex-col gap-5'>
+
+    <FormField
+    labelName = "Your name"
+    type = "text"
+    name = "name"
+    placeholder = "John Doe"
+    value = {form.name}
+    handleChange = {handleChange}
+    />
+
+    <FormField
+    labelName = "Prompt"
+    type = "text"
+    name = "prompt"
+    placeholder = "A plush toy robot sitting against a yellow wall"
+    value = {form.prompt}
+    handleChange = {handleChange}
+    isSurpriseMe
+    handleSurpriseMe = {handleSurpriseMe}
+    />
+   <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+    focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            { form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+       
+    
+
+    </div>
+
+    </form>
+
+
+
     </section>
   )
 }
